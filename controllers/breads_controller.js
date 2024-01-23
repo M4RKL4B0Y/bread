6b8c1346af67b8cfa209b424fc02c6da22da5eb0
M4RKL4B0Y <markvlaboy@gmail.com>
2024-01-23 00:13:57 -0500

    index and data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,9 +2,14 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-//index
+//INDEX
 breads.get('/', (req, res) => {
-    res.send ('Bread')
+    res.render ('Index',
+    {
+        breads: Bread
+    }
+    )
+    // res.send(Bread)
 })
 
 //SHOW
