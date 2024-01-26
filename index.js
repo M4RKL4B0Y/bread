79048190c2afee62d8f7483a03ffbbbf918857c4
M4RKL4B0Y <markvlaboy@gmail.com>
2024-01-25 19:18:25 -0500

    delete button and method

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // DEPENDENCIES
 const express = require('express')
 
+// DEPENDENCIES
+const methodOverride = require('method-override')
+
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -12,7 +15,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req, res) => {
--- views/new.jsx
+++ views/new.jsx
@@ -28,6 +28,12 @@ function New () {
           <br />
           <input type="submit"/>
         </form>
+
+        <div className="backButton">
+           <a href="/breads"><button>Go back to the index</button></a>
+        </div>
+
+
       </Default>
     )
 }
