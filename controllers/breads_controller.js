0342eb1866397b64fd7587e6e278607c87fe56ac
M4RKL4B0Y <markvlaboy@gmail.com>
2024-02-11 15:31:38 -0500

    started from scratch

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,112 +1,15 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
-const Baker = require('../models/baker.js')
 
-//INDEX
+// INDEX
 breads.get('/', (req, res) => {
-  Bread.find()
-    .then(foundBreads => {
-      // console.log(foundBreads)
-    
-      res.render('index', {
-        breads: foundBreads, // Pass foundBreads, not Bread
-        title: 'Index Page'
-      });
-    })
-    //In case route has trouble rendering
-    // .catch(error => {
-    //   console.error('Error fetching breads:', error);
-    //   res.status(500).send('Internal Server Error'); // Handle error response
-    // });
+  res.send(Bread)
 })
-
-// NEW
-breads.get('/new', (req, res) => {
-  Baker.find()
-      .then(foundBakers => {
-          res.render('new', {
-              bakers: foundBakers
-          })
-    })
-})
-// breads.get('/new', (req, res) => {
-//     res.render('new')
-// })
-
-// EDIT
-breads.get('/:id/edit', (req, res) => {
-  Baker.find()
-    .then(foundBakers => {
-  Bread.findById(req.params.id)
-    .then(foundBread => {
-       res.render('edit', {
-        bread: foundBread,
-        bakers: foundBakers
-          // index: req.params.indexArray
-       })
-    })
-  })
-})
-
 
 // SHOW
-breads.get('/:id', (req, res) => {
-  Bread.findById(req.params.id)
-      .populate('baker')
-      .then(foundBread => {
-        // const bakedBy = foundBread.getBakedBy()
-        // console.log(bakedBy)
-          res.render('Show', {
-              bread: foundBread
-          })
-          
-      })
-   
+breads.get('/:arrayIndex', (req, res) => {
+  res.send(Bread[req.params.arrayIndex])
 })
-
-
-  // CREATE
-breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-  }
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread.push(req.body)
-  res.redirect('/breads')
-})
-
-  
-
-  
-  // DELETE
-breads.delete('/:id', (req, res) => {
-  Bread.findByIdAndDelete(req.params.id)
-  .then(deletedBread => {
-    res.status(303).redirect('/breads')
-  })
-})
-
-// UPDATE
-breads.put('/:id', (req, res) => {
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then(updatedBread => {
-      console.log(updatedBread)
-    res.redirect(`/breads/${req.params.id}`)
-    })
-})
-
-
-  
 
 module.exports = breads
-
--- index.js
+++ index.js
@@ -1,48 +1,21 @@
 // DEPENDENCIES
 const express = require('express')
-const mongoose = require('mongoose')
-
-// DEPENDENCIES
-const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-// MIDDLEWARE
-// app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
-
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+  res.send('Welcome to an Awesome App about Breads')
 })
 
-//BREADS
+// Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
-
-//BAKERS
-const bakersController = require('./controllers/bakers_controller.js')
-app.use('/bakers', bakersController)
-
-//404 Page
-app.get('*', (req, res) => {
-  res.send('404')
-})
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
-
-
-// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-// () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-// )
